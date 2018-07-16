ce485761234cc5f816785444f9155cb6c50cacdc
Kelly Hondula <khondula@sesync.org>
2018-07-16 16:28:23 -0400

    update describe annotation function

--- R/db_describe_annotation.R
+++ R/db_describe_annotation.R
@@ -2,6 +2,7 @@
 #'
 #' @param annotationtypecv annotation type from controlled vocab
 #' @param annotationtext text string of annotation
+#' @param db database connection object
 #'
 #' @return TRUE if successful
 #' @export
@@ -11,20 +12,36 @@
 #' db_describe_annotaion("samplingfeature annotation", "primary sites")
 #' }
 #'
-db_describe_annotation <- function(annotationtypecv,
+db_describe_annotation <- function(db,
+                                   annotationtypecv,
                                   annotationtext){
-  sql <- sprintf('INSERT INTO odm2.annotations (annotationtypecv, annotationtext)
-                 VALUES (\'%s\', \'%s\')',
-                 annotationtypecv, annotationtext)
-  sql <- gsub("\n", "", sql)
-  RPostgreSQL::dbGetQuery(db, sql)
-}
 
-db_describe_annotation_sqlite <- function(annotationtypecv,
-                              annotationtext){
-  sql <- sprintf('INSERT INTO annotations (annotationtypecv, annotationtext)
-                 VALUES (\'%s\', \'%s\')',
-                 annotationtypecv, annotationtext)
-  sql <- gsub("\n", "", sql)
-  dbExecute(db, sql)
+  if (!class(db) %in% c("SQLiteConnection", "PostgreSQLConnection")) {
+    stop("sorry, only sqlite and postgresql database connections are supported so far")}
+
+  # check type of database object
+  if (class(db) == "SQLiteConnection"){
+    sql <- RSQLite::dbSendStatement(db,
+  'INSERT INTO annotations (annotationtypecv, annotationtext)
+                 VALUES (:annotationtypecv, :annotationtext)')
+    RSQLite::dbBind(sql, param = list(annotationtypecv = annotationtypecv,
+                                      annotationtext = annotationtext))
+    RSQLite::dbClearResult(res = sql)
+    message(paste(annotationtext, "has been entered into the annotations table."))
+  }
+
+  if (class(db) == "PostgreSQLConnection"){
+
+    sql <- DBI::sqlInterpolate(db,
+                               'INSERT into odm2.annotations
+                               (annotationtypecv, annotationtext)
+                               VALUES (?annotationtypecv, ?annotationtext)',
+                               annotationtypecv = annotationtypecv,
+                               annotationtext = annotationtext)
+
+    RPostgreSQL::dbGetQuery(db, sql)
+    message(paste(annotationtext, "has been entered into the annotations table."))
+
+  }
+
 }
