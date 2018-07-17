a0df628abc3cf778f8404a5875623201d61dde59
Kelly Hondula <khondula@sesync.org>
2018-07-17 16:34:20 -0400

    as character for postgres dates!

--- R/db_describe_equipment.R
+++ R/db_describe_equipment.R
@@ -203,7 +203,7 @@ db_describe_equipment <- function(db,
                                   equipmentserialnumber = serial_no,
                                   personfirstname = owner_first,
                                   vendorname = vendor,
-                                  equipmentpurchasedate = as.Date(purchase_date))
+                                  equipmentpurchasedate = as.character(purchase_date))
       RPostgreSQL::dbGetQuery(db, sql6)
       message(paste0(owner_first,"'s ", model_name, " ",
                      equipment_type, " ", equip_name,
