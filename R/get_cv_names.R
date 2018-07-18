6b74cf6ad2a94eef5cc523f3b67a219f7bbce7c6
Kelly Hondula <khondula@sesync.org>
2018-07-18 16:43:16 -0400

    change function name since it was confusing me

--- R/get_cv_names.R
+++ R/get_cv_names.R
@@ -2,24 +2,22 @@
 #'
 #' @param cvtype the name of the controlled vocab eg. "resulttype" or "methodtype"
 #'
-#' @return a vector of the Names column from the CV_%cvtype% table
+#' @return a vector of the Names column from the controlled vocab table
 #' @export
 #'
 #' @examples
-#' get_cv_names("resulttype")
-#' resultterms <- get_cv_names("resulttype")
-get_cv_names <- function(cvtype){
+#' get_cv_terms("resulttype")
+#' resultterms <- get_cv_terms("resulttype")
+get_cv_terms <- function(cvtype){
   tmp <- tempdir()
-  if(!exists("db")){
-    rodm2::create_sqlite(dir = tmp)
-    db <- DBI::dbConnect(RSQLite::SQLite(), file.path(tmp, "odm2.sqlite"))
-  }
+  db <- rodm2::create_sqlite(dir = tmp, connect = TRUE)
   sql <- DBI::sqlInterpolate(db, paste0("SELECT Name from cv_", cvtype))
   cv <- RSQLite::dbGetQuery(db, sql)
   cv_paste1 <- noquote(paste(cv))
   cv_paste <- gsub(cv_paste1, pattern = "\n", replacement = "", fixed = TRUE)
-  cv_sub <- substr(cv_paste, start = 3, stop = nchar(cv)-2)
+  cv_sub <- substr(cv_paste, start = 3, stop = nchar(cv)-1)
   cv_message <- paste(crayon::cyan(cvtype, "controlled vocabulary terms:"), cv_sub)
   writeLines(cv_message)
+  file.remove(file.path(tmp, "odm2.sqlite"))
   invisible(cv)
 }
