c1eac388bd47faae503f0b822eb72ee272b49d55
Kelly Hondula <khondula@sesync.org>
2018-07-30 15:25:41 -0400

    allow get cv function to handle units

--- R/get_cv_terms.R
+++ R/get_cv_terms.R
@@ -12,7 +12,11 @@ get_cv_terms <- function(cvtype){
   tmp <- tempdir()
   db <- rodm2::create_sqlite(dir = tmp, connect = TRUE)
   sql <- DBI::sqlInterpolate(db, paste0("SELECT Name from cv_", cvtype))
+  if(cvtype == "units"){
+    sql <- DBI::sqlInterpolate(db, "SELECT unitsname from units")
+  }
   cv <- RSQLite::dbGetQuery(db, sql)
+  RSQLite::dbDisconnect(db)
   cv_paste1 <- noquote(paste(cv))
   cv_paste <- gsub(cv_paste1, pattern = "\n", replacement = "", fixed = TRUE)
   cv_sub <- substr(cv_paste, start = 3, stop = nchar(cv)-1)
