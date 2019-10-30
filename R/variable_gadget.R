a022e9a822849b33be3be82356c8adb4d889f040
Kelly Hondula <khondula@sesync.org>
2019-10-30 15:19:48 -0400

    improvements to make vars list shiny gadget

--- R/variable_gadget.R
+++ R/variable_gadget.R
@@ -15,14 +15,15 @@
 make_vars_list <- function(data) {
 
   data_colnames <- setdiff(names(data), "Timestamp")
-  variablenamescv <- rodm2::get_cv_terms("variablename")
-  unitscv <- rodm2::get_cv_terms("units")
+  suppressMessages(variablenamescv <- rodm2::get_cv_terms("variablename"))
+  suppressMessages(unitscv <- rodm2::get_cv_terms("units"))
 
   ui <- miniUI::miniPage(
-    miniUI::gadgetTitleBar("Please describe the data in each column"),
+    miniUI::gadgetTitleBar("Specify code and controlled vocab (CV) for each column"),
     miniUI::miniContentPanel(
       # Define layout, inputs, outputs
       shiny::fillRow(
+        shiny::uiOutput('selectorsUIcode'),
         shiny::uiOutput('selectorsUIvar'),
         shiny::uiOutput('selectorsUIunits')
       )))
@@ -30,11 +31,21 @@ make_vars_list <- function(data) {
   server <- function(input, output, session) {
     # Define reactive expressions, outputs, etc.
     # drop down for variable selection for each column
+    output$selectorsUIcode <- shiny::renderUI({
+
+      lapply(1:length(data_colnames), function(i){
+        shiny::textInput(inputId = sprintf("var%scode", i),
+                              label = sprintf('Variable code for \"%s\"', data_colnames[i]),
+                              value = data_colnames[i])
+      })
+
+    })
+
     output$selectorsUIvar <- shiny::renderUI({
 
       lapply(1:length(data_colnames), function(i){
         shiny::selectizeInput(inputId = sprintf("var%sname", i),
-                       label = sprintf('Column %s variable name', data_colnames[i]),
+                       label = sprintf('CV term for \"%s\"', data_colnames[i]),
                        choices = variablenamescv,
                        options = list(placeholder = 'Start typing here',
                                       onInitialize = I('function() { this.setValue(""); }')))
@@ -45,7 +56,7 @@ make_vars_list <- function(data) {
       # drop down for units selection for each column
       lapply(1:length(data_colnames), function(i){
         shiny::selectizeInput(inputId = sprintf("var%sunits", i),
-                       label = sprintf('Column %s units', data_colnames[i]),
+                       label = sprintf('CV units for \"%s\"', data_colnames[i]),
                        choices = unitscv,
                        options = list(placeholder = 'Start typing here',
                                       onInitialize = I('function() { this.setValue(""); }')))
@@ -55,10 +66,12 @@ make_vars_list <- function(data) {
     # When the Done button is clicked, return a value
     shiny::observeEvent(input$done, {
       returnValue <- lapply(1:length(data_colnames), function(i){
-        list('column' = data_colnames[i], 'units' = input[[sprintf('var%sunits', i)]])
+        list('column' = data_colnames[i],
+             'name' = input[[sprintf('var%sname', i)]],
+             'units' = input[[sprintf('var%sunits', i)]])
       })
       names(returnValue) <- sapply(1:length(data_colnames), function(i){
-        input[[sprintf('var%sname', i)]]
+        input[[sprintf('var%scode', i)]]
       })
       shiny::stopApp(returnValue)
 
