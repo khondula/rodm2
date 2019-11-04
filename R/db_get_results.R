27221faaf51f7478d3e2e517d1f1f0d00eab2441
Kelly Hondula <khondula@sesync.org>
2019-11-04 18:55:09 -0500

    include measurement results as an option in db get results

--- R/db_get_results.R
+++ R/db_get_results.R
@@ -2,12 +2,12 @@
 
 #' Title
 #'
-#' @param db
-#' @param site_code
-#' @param variable_code
-#' @param result_type
+#' @param db database connection object
+#' @param site_code a string vector with codes of sites to get data for. default NULL returns all sites
+#' @param variable_code a string vector with codes of variables to get data for. default NULL returns all variables
+#' @param result_type a string vector of result types to include: "ts" for time series results, "sample" for sample-based results, "measurement" for measurements
 #'
-#' @return
+#' @return a named list of dataframes corresponding to each result type.
 #' @export
 #'
 #' @examples
@@ -20,9 +20,6 @@ db_get_results <- function(db,
   if (!class(db) %in% c("SQLiteConnection")) {
     stop("sorry, only sqlite database connections are supported so far")}
 
-  if("measurement" %in% result_type){
-    stop("to be implemented after lunch")
-  }
 
   if("profile" %in% result_type){
     stop("to be implemented after lunch")
@@ -45,6 +42,70 @@ db_get_results <- function(db,
     variable_code <- RSQLite::dbGetQuery(db, "SELECT variablecode from variables")
     variable_code <- unique(variable_code[[1]][!is.null(variable_code)])
   }
+
+  results_data_measurement <- c()
+
+  if("measurement" %in% result_type){
+    # Retreive Sampling Feature ID for site code
+    sf_id <- RSQLite::dbGetQuery(db,"SELECT SamplingFeatureID
+                                 FROM SamplingFeatures
+                                 WHERE SamplingFeatureCode IN (:x)",
+                                 params=list(x=site_code))
+
+    sf_id_integer <- as.integer(sf_id[["SamplingFeatureID"]])
+    sf_id_integer <- as.character(sf_id_integer)
+
+    #Retreive Feature Action ID[s] for site code
+    FeatureActionID <- RSQLite::dbGetQuery(db,
+                                           "SELECT FeatureActionID
+                                           FROM FeatureActions
+                                           WHERE SamplingFeatureID IN (:x)",
+                                           params=list(x=sf_id_integer))
+
+    fa_id_integer <- as.integer(FeatureActionID[["FeatureActionID"]])
+    fa_id_integer <- as.character(fa_id_integer)
+    # fa_id_integer <- sprintf('%s', fa_id_integer)
+    # fa_id_integer <- paste0(sQuote(fa_id_integer), collapse = ",")
+
+    # Retreive variable id for variables
+    var_id <- RSQLite::dbGetQuery(db,"SELECT variableid
+                                  FROM variables
+                                  WHERE variablecode IN (:x)",
+                                  params=list(x=variable_code))
+
+    var_id_integer <- as.integer(var_id[["VariableID"]])
+    var_id_integer <- as.character(var_id_integer)
+
+    #Retreive Result ID[s] for each feature action
+
+    resultids_fa <- RSQLite::dbGetQuery(db,
+                                        "SELECT ResultID FROM Results
+                                        WHERE FeatureActionID IN (:x)",
+                                        params = list(x = fa_id_integer))
+    resultids_var <- RSQLite::dbGetQuery(db,
+                                         "SELECT ResultID FROM Results
+                                         WHERE VariableID IN (:x)",
+                                         params = list(x = var_id_integer))
+
+
+    result_id_integer <- intersect(resultids_fa[[1]], resultids_var[[1]])
+
+    #Retreive Result values
+
+    results_data_measurement <- RSQLite::dbGetQuery(db,
+                                           "SELECT mrv.ValueDateTime, mrv.DataValue, units.unitsname,
+                                           var.variablenamecv, sf.samplingfeaturecode, pl.ProcessingLevelCode
+                                           FROM measurementresultvalues mrv
+                                           LEFT JOIN results res ON res.resultid = mrv.resultid
+                                           LEFT JOIN featureactions fa ON fa.featureactionid = res.featureactionid
+                                           LEFT JOIN samplingfeatures sf ON sf.samplingfeatureid = fa.samplingfeatureid
+                                           LEFT JOIN variables var ON var.variableid = res.variableid
+                                           LEFT JOIN units ON units.unitsid = res.unitsid
+                                           LEFT JOIN processinglevels pl ON pl.processinglevelid = res.processinglevelid
+                                           WHERE mrv.ResultID IN (:x)",
+                                           params=list(x=result_id_integer))
+    }
+
   results_data_ts <- c()
   if("ts" %in% result_type){
 
@@ -182,6 +243,7 @@ db_get_results <- function(db,
   }
 
   results_list <- list("Time series data" = results_data_ts,
-                       "Sample data" = results_data_samples)
+                       "Sample data" = results_data_samples,
+                       "Measurements data" = results_data_measurement)
   return(results_list)
 }
