5d06699e68bb3830bd8ba79d61c4934746090df5
Kelly Hondula <khondula@sesync.org>
2019-11-10 23:48:49 -0500

    new method and site handling

--- R/insert-time-series-results.R
+++ R/insert-time-series-results.R
@@ -73,12 +73,8 @@ db_insert_results_ts <- function(db,
   if (!class(db) %in% c("SQLiteConnection", "PostgreSQLConnection")) {
     stop("sorry, only sqlite and postgres database connections are supported so far")}
 
-  # check for method and add if not in there
-  if(!(methodcode %in% rodm2::db_get_methods(db))){
-    rodm2::db_describe_method(db,
-                              methodcode = methodcode,
-                              methodtypecv = methodtypecv)
-  }
+
+
 
 
   # check that all variables are in variables table
@@ -92,12 +88,61 @@ db_insert_results_ts <- function(db,
 
   # check type of database object
   if (class(db) == "SQLiteConnection"){
-    RSQLite::dbSendStatement(db, 'BEGIN TRANSACTION')
 
-    # make sure site is in sampling features table
-    if(!site_code %in% rodm2::db_get_sites(db)){
-      rodm2::db_describe_site(db, site_code)
+    all_methodtypes <- RSQLite::dbGetQuery(db, "SELECT term from cv_methodtype")[["Term"]]
+    while(!methodtypecv %in% all_methodtypes){
+      methodtypecv_id <- suppressMessages(menu(choices = all_methodtypes,
+                                               graphics = FALSE,
+                                               title = paste("Please select method type for", methodcode, "from CV")))
+      methodtypecv <- all_methodtypes[methodtypecv_id]
     }
+
+    # New method handling
+    all_methods <- RSQLite::dbGetQuery(db, "SELECT methodcode from methods")[[1]]
+    if(!methodcode %in% all_methods){
+      selection_id <- suppressMessages(menu(choices = c(all_methods, paste("Add", methodcode,"as new method")),
+                                            graphics = FALSE,
+                                            title = paste("Method code not in database. Select option below or type 0 to quit: ")))
+      if (selection_id == 0) {
+        stop("See existing method codes using db_get_methods()")
+        } else if (selection_id == length(all_methods)+1) {
+          rodm2::db_describe_method(db,
+                                    methodcode = methodcode,
+                                    methodname = readline("Supply new method name: "),
+                                    methodtypecv = methodtypecv)
+      } else {
+        methodcode <- all_methods[selection_id]
+      }
+
+    }
+
+    # New site handling
+    all_site <- RSQLite::dbGetQuery(db, "SELECT samplingfeaturecode from samplingfeatures")[[1]]
+    if(!site_code %in% all_site){
+      selection_id <- suppressMessages(menu(choices = c(all_site, paste("Add", site_code,"as new site")),
+                                            graphics = FALSE,
+                                            title = paste("Site code not in database. Select option below or type 0 to quit: ")))
+      if (selection_id == 0) {
+        stop("See existing method codes using db_get_sites()")
+      } else if (selection_id == length(all_site)+1) {
+        if(!exists("sitetypecv")){
+          all_sftypes <- RSQLite::dbGetQuery(db, "SELECT term from cv_samplingfeaturetype")[["Term"]]
+            selection_id <- suppressMessages(menu(choices = all_sftypes,
+                                                  graphics = FALSE,
+                                                  title = paste("Please select site type for", site_code, "from CV")))
+            sitetypecv <- all_sftypes[selection_id]
+        }
+        rodm2::db_describe_site(db,
+                                  site_code = site_code,
+                                  site_name = readline(paste("Supply site name for new site code", site_code,": ")),
+                                  sitetypecv = sitetypecv)
+      } else {
+        site_code <- all_site[selection_id]
+      }
+
+    }
+
+    RSQLite::dbSendStatement(db, 'BEGIN TRANSACTION')
 
     # make sure processing level is in processinglevel table
     sql <- "INSERT or IGNORE into processinglevels (processinglevelcode) VALUES (:processinglevel)"
