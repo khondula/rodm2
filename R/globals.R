ae00965f4501b6018f6e6ce46205b4ab240b4fb3
Kelly Hondula <khondula@sesync.org>
2019-11-16 22:48:42 -0500

    globals function to take care of no global binding

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c("SamplingFeatureID",
+                         "RelatedFeatureID",
+                         "Timestamp",
+                         "valuedatetime",
+                         "zlocationunitsid",
+                         "ResultTypeCV"))
