00e088600970dc732b113552ac01bd453f984497
Kelly Hondula <khondula@sesync.org>
2019-11-22 22:25:59 -0500

    reorder so more common cvs are shown in tooltips

--- R/get_cv_terms.R
+++ R/get_cv_terms.R
@@ -9,15 +9,17 @@
 #' @examples
 #' get_cv_terms("resulttype")
 #' resultterms <- get_cv_terms("resulttype")
-get_cv_terms <- function(cvtype = c("actiontype", "aggregationstatistic", "annotationtype",
+get_cv_terms <- function(cvtype = c("units", "variablename", "samplingfeaturetype",
+                                    "medium", "methodtype", "actiontype", "annotationtype",
+                                    "relationshiptype",
                                     "censorcode", "dataqualitytype", "datasettype",
                                     "directivetype", "elevationdatum", "equipmenttype",
-                                    "medium", "methodtype", "organizationtype",
-                                    "propertydatatype", "qualitycode", "relationshiptype",
-                                    "resulttype", "samplingfeaturegeotype", "samplingfeaturetype",
+                                    "organizationtype", "aggregationstatistic",
+                                    "propertydatatype", "qualitycode",
+                                    "resulttype", "samplingfeaturegeotype",
                                     "sitetype", "spatialoffsettype", "speciation", "specimentype",
                                     "status", "taxonomicclassifiertype", "unitstype",
-                                    "variablename", "variabletype"), quietly = FALSE){
+                                    "variabletype"), quietly = FALSE){
   tmp <- tempdir()
   if(file.exists(file.path(tmp, "odm2.sqlite"))){
     db <- rodm2::connect_sqlite()
